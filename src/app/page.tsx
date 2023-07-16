5e5f6fba251eca8f961fb54839857f6e04f049bf
LordPrinz <plazinski2902@gmail.com>
2023-07-16 18:37:50 +0200

    Fill layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Announcement from "@/components/Home/Announcement";
+import Games from "@/components/Home/Games";
 import Header from "@/components/Home/Header";
 import Navigation from "@/components/Home/Navigation";
+import Blog from "@/components/Home/Blog";
+import Reviews from "@/components/Home/Reviews";
+import Newsletter from "@/components/Home/Newsletter";
+import Footer from "@/components/Home/Footer";
 
 export default function Home() {
 	return (
@@ -8,6 +13,11 @@ export default function Home() {
 			<Navigation />
 			<Header />
 			<Announcement />
+			<Games />
+			<Blog />
+			<Reviews />
+			<Newsletter />
+			<Footer />
 		</div>
 	);
 }
--- src/components/Home/News.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const News = () => {
-	return <div>News</div>;
-};
-
-export default News;
