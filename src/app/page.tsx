1cbd3a3862967e25b747454b15f630b3d802f9f1
LordPrinz <plazinski2902@gmail.com>
2023-07-16 18:27:38 +0200

    Create Games component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Navigation from "@/components/Navigation";
-import Image from "next/image";
+import Announcement from "@/components/Home/Announcement";
+import Header from "@/components/Home/Header";
+import Navigation from "@/components/Home/Navigation";
 
 export default function Home() {
 	return (
 		<div className="main-page-layout">
 			<Navigation />
+			<Header />
+			<Announcement />
 		</div>
 	);
 }
