485578d85e6964e8e402af1d60d9141d82e1bf63
LordPrinz <plazinski2902@gmail.com>
2023-07-16 22:06:14 +0200

    Create useDarkModeHook

---
+++ src/hooks/useDarkMode.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = (): [boolean, (enabled: boolean) => void] => {
+	const [enabled, setEnabled] = useLocalStorage("dark-theme", false);
+	const isEnabled = typeof enabled === "undefined" ? enabled : enabled;
+
+	useEffect(() => {
+		const className = "dark";
+		const bodyClass = window.document.body.classList;
+
+		isEnabled ? bodyClass.add(className) : bodyClass.remove(className);
+	}, [isEnabled]);
+
+	return [enabled, setEnabled];
+};
+
+export default useDarkMode;
