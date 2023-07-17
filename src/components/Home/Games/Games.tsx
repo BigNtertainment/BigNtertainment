05c2cfdbcb6bf3a86b8c956971e132b2d73d19c2
LordPrinz <plazinski2902@gmail.com>
2023-07-17 16:59:34 +0200

    Create games section

--- src/components/Home/Games/Games.tsx
+++ src/components/Home/Games/Games.tsx
@@ -3,9 +3,10 @@ import GamesList from "./GamesList";
 
 const Games = () => {
 	return (
-		<div className="col-[center-start/center-end] my-36 ">
+		<div className="col-[center-start/center-end] my-36 grid">
 			<h3 className="text-6xl font-bold text-center mb-32">Our Games</h3>
 			<GamesList />
+			<button className="mt-16 mx-auto">See more games</button>
 		</div>
 	);
 };
