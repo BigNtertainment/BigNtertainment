f33a3f8142c5f4bc78fa7f368f59d182e6212c68
LordPrinz <plazinski2902@gmail.com>
2023-07-17 23:52:18 +0200

    Style spinner

--- src/components/Home/Header/Slider.tsx
+++ src/components/Home/Header/Slider.tsx
@@ -1,31 +1,18 @@
 "use client";
 
+import Image from "next/image";
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 
-const spanStyle = {
-	padding: "20px",
-	background: "#efefef",
-	color: "#000000",
-};
-
-const divStyle = {
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
-	backgroundSize: "cover",
-	height: "100%",
-	"background-attachment": "fixed",
-	top: 0,
-};
 const slideImages = [
 	{
 		url: "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
 		caption: "Slide 1",
 	},
 	{
-		url: "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
-		caption: "Slide 2",
+		url: "https://img.itch.zone/aW1hZ2UvMjE2MDc2OS8xMjc1MTkxNS5wbmc=/original/yJay1h.png",
+		title: "golf-control",
+		description: "",
 	},
 	{
 		url: "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
@@ -45,11 +32,23 @@ const Slideshow = () => {
 				{slideImages.map((slideImage, index) => (
 					<div key={index} className="h-full ">
 						<div
-							className="h-full "
+							className="h-full bg-cover grid grid-cols-2"
 							style={{
-								...divStyle,
-								backgroundImage: `linear-gradient(105deg,rgba(255,255,255,.9) 0,rgba(255,255,255,.9) 45%,transparent 45%),url(${slideImage.url})`,
-							}}></div>
+								backgroundImage: `linear-gradient(105deg,rgba(37,37,37,.9) 0,rgba(37,37,37,.9) 45%,transparent 45%),url(${slideImage.url})`,
+							}}>
+							<div>
+								<div className="uppercase flex flex-col gap-8 items-center h-full flex-1 mt-32">
+									{/* BigNtertainment */}
+									{/* <Image
+										width={500}
+										height={100}
+										alt="aaa"
+										src={`/${slideImage.title}.png`}
+									/> */}
+									{/* <h2 className="text-4xl text-center ">{slideImage.description}</h2> */}
+								</div>
+							</div>
+						</div>
 					</div>
 				))}
 			</Slide>
