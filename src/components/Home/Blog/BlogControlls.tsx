850def1033c6f05c53af98336ed7a6c51479f58a
LordPrinz <plazinski2902@gmail.com>
2023-07-18 18:47:35 +0200

    Implement tag search

--- src/components/Home/Blog/BlogControlls.tsx
+++ src/components/Home/Blog/BlogControlls.tsx
@@ -1,26 +1,31 @@
-"use client";
+// BlogControlls.js
 
 import React, { useEffect } from "react";
 import BlogOption from "./BlogOption";
 
 type Props = {
+	options: string[];
 	selectedOption: string;
 	setSelectedOption: (option: string) => void;
 };
 
-const DUMMY_DATA = ["Latest", "Golf Control", "Fifa 23"];
+const DEFAULT_OPTION = "Latest";
 
-const BlogControlls = ({ selectedOption, setSelectedOption }: Props) => {
+const BlogControlls = ({
+	selectedOption,
+	setSelectedOption,
+	options,
+}: Props) => {
 	useEffect(() => {
 		if (selectedOption === "") {
-			setSelectedOption(DUMMY_DATA[0]);
+			setSelectedOption(DEFAULT_OPTION);
 		}
 	}, []);
 
 	return (
 		<div className="col-[1/-1] relative after:border-t-2 after:w-full after:absolute after:bottom-0 after:left-0 after:bg-dark-primary after:opacity-[0.08]">
 			<ul className="flex justify-center">
-				{DUMMY_DATA.map((game) => {
+				{[DEFAULT_OPTION, ...options].map((game) => {
 					return (
 						<BlogOption
 							key={game}
--- src/components/Home/Blog/BlogList.tsx
+++ src/components/Home/Blog/BlogList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import BlogControlls from "./BlogControlls";
 import BlogInfo from "./BlogInfo";
 import Btn from "@/components/util/Btn";
@@ -10,8 +10,20 @@ import { BlogPost } from "@/types/INotion";
 
 const BlogList = () => {
 	const [selectedOption, setSelectedOption] = useState("");
-
+	const [uniqueTags, setUniqueTags] = useState<string[]>([]);
 	const { data } = useSWR("/api/posts?limit=6", fetcher);
+
+	useEffect(() => {
+		if (data) {
+			const posts = data.data as BlogPost[];
+
+			const tags = posts.flatMap((post) =>
+				post.tags.map((tag) => tag.name.replace("#", ""))
+			);
+
+			setUniqueTags([...new Set(tags)]);
+		}
+	}, [data]);
 
 	let listToRender: React.JSX.Element | React.JSX.Element[] = (
 		<div className="text-5xl mb-4 font-bold text-center col-[2/-2]">
@@ -21,12 +33,14 @@ const BlogList = () => {
 
 	if (data) {
 		const posts = data.data as BlogPost[];
+
 		listToRender = posts.map((post) => <BlogInfo key={post.id} />);
 	}
 
 	return (
 		<div className="grid grid-cols-[minmax(6rem,1fr)_repeat(8,minmax(min-content,14rem))_minmax(6rem,1fr)_]">
 			<BlogControlls
+				options={uniqueTags}
 				selectedOption={selectedOption}
 				setSelectedOption={setSelectedOption}
 			/>
