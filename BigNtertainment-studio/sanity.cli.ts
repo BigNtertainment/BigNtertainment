aa64f1bf5d4e088a7c071ad4330a8d195cce1aeb
LordPrinz <plazinski2902@gmail.com>
2023-07-24 20:49:22 +0200

    Add Sanity project

---
+++ BigNtertainment-studio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'e0wxu9kd',
+    dataset: 'production'
+  }
+})
--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -5,7 +5,6 @@ const page = () => {
 	return (
 		<div className="col-[center-start/center-end] pb-20">
 			<h3 className="text-6xl font-bold text-center mb-32 mt-24">Our Games</h3>
-
 			<GamesList />
 		</div>
 	);
