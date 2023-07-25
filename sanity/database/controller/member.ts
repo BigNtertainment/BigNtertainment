e5f6920aa4f113694a9fa88060e2fc750e71f83f
LordPrinz <plazinski2902@gmail.com>
2023-07-25 23:36:17 +0200

    Implement get photo method

--- sanity/database/controller/member.ts
+++ sanity/database/controller/member.ts
@@ -1,11 +1,38 @@
-import { SanityClient } from "next-sanity";
+import { SanityClient } from "sanity";
+import { getPhoto } from "../../utils/getExtra";
 
-export type memberQuery = {
-	getAll: () => void;
+export type member = {
+	id: string;
+	name: string;
+	surname: string;
+	personalPageLink: string;
+	description: string;
+	slug: string;
+	image: string;
 };
 
-export function getAll(this: any) {
-	const { client } = this;
+export type memberQuery = {
+	getAll: () => Promise<member[]>;
+};
 
-	console.log(client);
+const transformMember = (members: any[]): member[] => {
+	return members.map((member) => ({
+		id: member._id,
+		name: member.firstName,
+		surname: member.surname,
+		personalPageLink: member.personalPageLink,
+		description: member.description,
+		slug: member?.slug?.current,
+		image: getPhoto(member.image.asset._ref),
+	}));
+};
+
+export async function getAll(this: any) {
+	const client = this.client as SanityClient;
+
+	const query = `*[_type == "member"]`;
+
+	const data = await client.fetch(query);
+
+	return transformMember(data);
 }
