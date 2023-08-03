849a4c341979bf5318119233864fe0a20a72031f
LordPrinz <plazinski2902@gmail.com>
2023-08-03 21:12:09 +0200

    Create contact page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	experimental: {
+		serverActions: true,
+	},
 	images: {
 		domains: ["www.notion.so", "img.itch.zone", "cdn.sanity.io"],
 	},
