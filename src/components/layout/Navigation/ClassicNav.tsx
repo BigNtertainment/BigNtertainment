3f1d318b9c795d8f43a25b927a4142d54e6d616b
LordPrinz <plazinski2902@gmail.com>
2023-08-04 16:15:12 +0200

    Add responsive to logo

--- src/components/layout/Navigation/ClassicNav.tsx
+++ src/components/layout/Navigation/ClassicNav.tsx
@@ -6,13 +6,11 @@ import NavAuth from "./NavAuth";
 const ClassicNav = () => {
 	return (
 		<>
-			<div className="h-full flex gap-6 ">
+			<div className="h-full flex gap-3 ">
 				<div className="relative h-full aspect-square ">
-					<Logo fill={true} className="opacity-50" />
+					<Logo className="opacity-60" height={48} width={48} />
 				</div>
-				<div className="flex items-center translate-y-0.5 -translate-x-3 text-xl py-1.5 lg:p-0">
-					BigNtertainment
-				</div>
+
 				<NavLinks className={"hidden lg:flex"} />
 			</div>
 			<div className="gap-10 hidden lg:flex">
