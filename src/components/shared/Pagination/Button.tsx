2487382c4071354613c4ab429bd1a0321f06fd84
LordPrinz <plazinski2902@gmail.com>
2023-08-06 18:32:15 +0200

    Create Paginator Component

--- src/components/shared/Pagination/Button.tsx
+++ src/components/shared/Pagination/Button.tsx
@@ -13,14 +13,14 @@ const Button = ({ children, className, onClick, disabled, active }: Props) => {
 	const disabledClass = "opacity-20 hover:bg-transparent";
 
 	const activeClass =
-		"!bg-dark-theme hover:transition transition hover:shadow-md";
+		"!bg-[#2a2a2a] hover:transition transition hover:shadow-md text-dark-primary";
 
 	return (
 		<button
 			disabled={disabled}
 			onClick={onClick}
 			className={cn(
-				"flex items-center gap-1 px-5 py-3 rounded-3xl  transition hover:transition  hover:bg-dark-line-highlight",
+				"flex items-center gap-1 px-5 py-3 rounded-3xl text-dark-secondary  transition hover:transition   hover:bg-dark-line-highlight",
 				disabled ? disabledClass : "",
 				active ? activeClass : "",
 				className
