ee8ddcb9f8cf9d1cc28332c4f83dc4084cffc055
LordPrinz <plazinski2902@gmail.com>
2023-08-07 23:10:04 +0200

    Solve Link bug

--- src/components/About/Member.tsx
+++ src/components/About/Member.tsx
@@ -9,7 +9,7 @@ type Props = {
 const MemberComponent = ({ member }: Props) => {
 	return (
 		<Link
-			href={member.personalPageLink}
+			href={member.personalPageLink ?? "#"}
 			className="rounded-3xl bg-dark-line-highlight hover:-translate-y-1 transition hover:transition">
 			<div className="relative aspect-square rounded-t-3xl ">
 				<Image
