fbea7274a1af9414244f14cd4f5ea861c11d9a27
LordPrinz <61083743+LordPrinz@users.noreply.github.com>
2023-08-08 01:00:07 +0200

    Make Logo as a Link to homepage

--- src/components/layout/Navigation/ClassicNav.tsx
+++ src/components/layout/Navigation/ClassicNav.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import NavLinks from "./NavLinks";
 import Logo from "@/components/shared/Logo";
+import Link from "next/link";
 
 const ClassicNav = () => {
 	return (
 		<>
 			<div className="h-full flex gap-3 ">
-				<div className="relative h-full aspect-square w-11 lg:w-12 ">
+				<Link href="/" className="relative h-full aspect-square w-11 lg:w-12 ">
 					<Logo className="opacity-50" fill={true} />
-				</div>
+				</Link>
 
 				<NavLinks className={"hidden lg:flex"} />
 			</div>
