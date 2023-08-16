268794bd713b5aed2e1a8ec84a7a7bf3c5b4c16f
LordPrinz <plazinski2902@gmail.com>
2023-08-16 23:54:17 +0200

    Add Games Loader

---
+++ src/components/shared/loading/Game.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Game = () => {
+	return (
+		<div
+			role="status"
+			className="rounded-3xl  animate-pulse bg-dark-loading"></div>
+	);
+};
+
+export default Game;
